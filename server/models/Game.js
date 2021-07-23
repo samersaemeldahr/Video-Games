6a2326979bfa69de450e438c9976547e6152e873
Samer Saem Eldahr <samersaemeldahr@gmail.com>
2021-07-23 00:53:25 -0500

    Edit game schema

--- server/models/Game.js
+++ server/models/Game.js
@@ -2,40 +2,31 @@ const { Schema } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
 const gameSchema = new Schema({
-  name: [
-    {
-      type: String,
-      required: true,
-    },
-  ],
+  name: {
+    type: String,
+    required: true,
+  },
   released: {
     type: String,
   },
   background_image: {
     type: String,
   },
-rating: {
-  type: Integer,
-},
-metacritic: {
-  type:Integer,
-},
+  rating: {
+    type: Integer,
+  },
+  metacritic: {
+    type: Integer,
+  },
 
   // saved game id from rawg - changed bookId to gameId
   gameId: {
     type: String,
     required: true,
   },
-  image: {
+  platform: [{
     type: String,
-  },
-  link: {
-    type: String,
-  },
-  title: {
-    type: String,
-    required: true,
-  },
+  }],
 });
 
 module.exports = gameSchema;
