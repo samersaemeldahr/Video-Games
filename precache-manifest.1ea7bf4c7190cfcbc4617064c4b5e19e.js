d3d9860c0ed4d680fb7df259f576d0eb405299d6
nicholas <nicholaspheld@gmail.com>
2021-07-28 18:46:39 -0500

    Updates

---
+++ precache-manifest.1ea7bf4c7190cfcbc4617064c4b5e19e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48293c653cdcf1c91819c5f09cc97d59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c69f10bfacd87ee0b72f",
+    "url": "/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "2bc21f8c508c4804301f",
+    "url": "/static/css/main.196762a4.chunk.css"
+  },
+  {
+    "revision": "c69f10bfacd87ee0b72f",
+    "url": "/static/js/2.ca0e1949.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.ca0e1949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc21f8c508c4804301f",
+    "url": "/static/js/main.9e5e004f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
