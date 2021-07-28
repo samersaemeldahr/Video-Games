d52ec4a5f4d5369e0c31b20ed3b7ea8a451eefbc
nicholas <nicholaspheld@gmail.com>
2021-07-28 12:07:29 -0500

    Updates

---
+++ precache-manifest.1bf7b8bc856e44f85dd51829dc5b6406.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55bb91ef76dcb7749d9cf52dd2b51705",
+    "url": "/Video-Games/index.html"
+  },
+  {
+    "revision": "382942ea73f9b3b69c72",
+    "url": "/Video-Games/static/css/2.63f31f8d.chunk.css"
+  },
+  {
+    "revision": "d63baea5a0ad6ee67705",
+    "url": "/Video-Games/static/css/main.196762a4.chunk.css"
+  },
+  {
+    "revision": "382942ea73f9b3b69c72",
+    "url": "/Video-Games/static/js/2.054a3dc8.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/Video-Games/static/js/2.054a3dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d63baea5a0ad6ee67705",
+    "url": "/Video-Games/static/js/main.740dde1a.chunk.js"
+  },
+  {
+    "revision": "3593fad5e8850ec43963",
+    "url": "/Video-Games/static/js/runtime-main.ef46c37b.js"
+  }
+]);
